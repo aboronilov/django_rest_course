2eeb6a32afeaeb2b81e5347de673b315f0a94b2c
= <=>
2022-02-07 10:35:55 +0200

    lesson 4 - done

--- drfsite/women/serializers.py
+++ drfsite/women/serializers.py
@@ -1,8 +1,38 @@
+import io
+
 from rest_framework import serializers
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+
 from .models import Women
 
+#
+# class WomenModel:
+#     def __init__(self, title, content):
+#         self.title = title
+#         self.content = content
 
-class WomenSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Women
-        fields = ('title', 'cat_id')
+
+class WomenSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
+    is_published = serializers.BooleanField(default=True)
+    cat_id = serializers.IntegerField()
+
+
+# def encode():
+#     model = WomenModel('Angelina Jolie', 'Angelina Jolie content')
+#     women_sr = WomenSerializer(model)
+#     print(women_sr.data, type(women_sr.data), sep='\n')
+#     json = JSONRenderer().render(women_sr.data)
+#     print(json)
+#
+#
+# def decode():
+#     stream = io.BytesIO(b'{"title":"Angelina Jolie","content":"Angelina Jolie content"}')
+#     data = JSONParser().parse(stream)
+#     serializer = WomenSerializer(data=data)
+#     serializer.is_valid()
+#     print(serializer.validated_data)
