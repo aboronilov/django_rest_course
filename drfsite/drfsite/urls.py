727638fc258900bcf0a11892e8ffa051f235dbd4
= <=>
2022-02-11 09:48:46 +0200

    lesson_8 ModelViewSet - OK

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -15,12 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from women.views import WomenListCreateAPIView, WomenUpdateAPIView, WomenDetailAPIView
+from rest_framework import routers
+from women.views import WomenModelViewSet
+
+router = routers.SimpleRouter()
+router.register('women', WomenModelViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/womenlist/', WomenListCreateAPIView.as_view()),
-    path('api/v1/womenlist/<int:pk>/', WomenUpdateAPIView.as_view()),
-    path('api/v1/womendetail/<int:pk>/', WomenDetailAPIView.as_view()),
+    path('api/v1/', include(router.urls))
+    # path('api/v1/womenlist/<int:pk>/', WomenUpdateAPIView.as_view()),
+    # path('api/v1/womendetail/<int:pk>/', WomenDetailAPIView.as_view()),
 
 ]
--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,53 +1,28 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Women
 from .serializers import WomenSerializer
 
 
-class WomenListCreateAPIView(generics.ListCreateAPIView):
+class WomenModelViewSet(viewsets.ModelViewSet):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
 
 
-class WomenUpdateAPIView(generics.UpdateAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
-
-
-class WomenDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
-
-
-# class WomenAPIView(APIView):
-#     def get(self, request):
-#         w = Women.objects.all()
-#         return Response({'posts': WomenSerializer(w, many=True).data})
-#
-#     def post(self, request):
-#         serializer = WomenSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-#
-#         return Response({'post': serializer.data})
-#
-#     def put(self, request, *args, **kwargs):
-#         pk = kwargs.get('pk', None)
-#         if not pk:
-#             return Response({"error": "Method PUT is not allowed"})
-#
-#         try:
-#             instance = Women.objects.get(pk=pk)
-#         except:
-#             return Response({"error": "Method PUT is not allowed"})
-#
-#         serializer = WomenSerializer(data=request.data, instance=instance)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-#         return Response({'post': serializer.data})
-
-
-# class WomenAPIView(generics.ListAPIView):
+# class WomenListCreateAPIView(generics.ListCreateAPIView):
 #     queryset = Women.objects.all()
 #     serializer_class = WomenSerializer
+#
+#
+# class WomenUpdateAPIView(generics.UpdateAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
+#
+#
+# class WomenDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
+
+
+
