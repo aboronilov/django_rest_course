e3fcae1e07a62bb45ad4c0ffd0a77dd9b7319d58
= <=>
2022-02-22 09:20:41 +0200

    lesson_15

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics, viewsets
 from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -9,10 +10,17 @@ from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
 from .serializers import WomenSerializer
 
 
+class WomenListCreateAPIPagination(PageNumberPagination):
+    page_size = 3
+    page_size_query_param = 'page_size'
+    max_page_size = 5
+
+
 class WomenListCreateAPIView(generics.ListCreateAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
     permission_classes = (IsAuthenticatedOrReadOnly, )
+    pagination_class = WomenListCreateAPIPagination
 
 
 class WomenUpdateAPIView(generics.RetrieveUpdateAPIView):
